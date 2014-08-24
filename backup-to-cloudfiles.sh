22a6b815d1bdd1f50cc7fc7aa88a71751f355b07
Michael Davies <michael@the-davies.net>
2014-08-24 19:04:30 +0930

    Fixing hostname differences on network location

--- backup-to-cloudfiles.sh
+++ backup-to-cloudfiles.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+HOSTN=$(hostname | cut -f 1 -d'.')
 cd ${HOME}
 for item in photos video
 do
-  ${HOME}/src/cloudfiles-tools/push_to_cloudfiles.py file://$item ord://backup/${HOSTNAME}/$item
+  ${HOME}/src/cloudfiles-tools/push_to_cloudfiles.py file://$item ord://backup/${HOSTN}/$item
 done
