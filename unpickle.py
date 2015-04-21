12cb20835e08b426a8e967a4dc273a6a15645b71
Michael Davies <michael@the-davies.net>
2015-04-21 13:58:33 +0930

    Adding a simple unpickler

---
+++ unpickle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+# unpickle.py - printty print the dump of a python pickle
+#
+# Copyright (C) 2015 Michael Davies (michael@the-davies.net)
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+import os
+import pickle
+import pprint
+import sys
+
+
+if len(sys.argv) == 1:
+    exit("%s: Please specify a file" % os.path.basename(__file__))
+
+pp = pprint.PrettyPrinter(indent=4)
+
+for f in sys.argv[1:]:
+    pp.pprint(pickle.load(open(f)))
