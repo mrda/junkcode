93d0d83195a58b3dc1c5e29cacf29752484824bb
Michael Davies <michael@the-davies.net>
2015-09-30 08:16:34 +0930

    Fixing copyright header

--- maze_binary.py
+++ maze_binary.py
@@ -2,6 +2,8 @@
 #
 # maze_binary.py <nums rows> <num cols> - build a maze using binary tree
 #
+# Copyright (C) 2015 Michael Davies <michael@the-davies.net>
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 2 of the
