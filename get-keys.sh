d8b222625ab29e060123c7b5e5cdac3e503f095e
Michael Davies <michael@the-davies.net>
2016-09-06 18:58:00 +0930

    Adding shortcut for retrieving github pub keys

---
+++ get-keys.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+#
+# get-keys.sh - retrieve the Github keys for a user
+#
+# Copyright (C) 2016 Michael Davies <michael@the-davies.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+USAGE="$(basename $0): <GitHub User>"
+
+if [ "$#" -eq 0 ]; then
+  echo ${USAGE}
+  exit 2
+fi
+
+KEYNAME=$1.keys
+
+if [ "$#" -eq 1 ]; then
+    curl -O https://github.com/${KEYNAME}
+    echo "$1's GitHub keys have been saved as ${KEYNAME}"
+    exit 0
+fi
+
