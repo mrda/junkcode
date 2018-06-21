415f45d9fd142e7e8d4d6c13e289392a28900899
Michael Davies <michael@the-davies.net>
2018-06-21 11:40:16 +0930

    List comprehension fizzbuzz for mikal

---
+++ fizzbuzz4.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#
+# fizzbuzz3.py - standard interview question solution from
+#   "Coding Horror" - See
+#   http://blog.codinghorror.com/why-cant-programmers-program/
+#
+# Copyright (C) 2018 Michael Davies <michael@the-davies.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+# Or try here: http://www.fsf.org/copyleft/gpl.html
+#
+from __future__ import print_function
+import sys
+
+
+def fizzbuzz(start, end):
+    print("\n".join(["Fizz"*(not i % 3) + "Buzz"*(not i % 5) or str(i)
+          for i in range(start, end)]))
+
+
+if __name__ == '__main__':
+    if len(sys.argv) == 3:
+        start = int(sys.argv[1])
+        end = int(sys.argv[2])
+    else:
+        start = 1
+        end = 100
+    fizzbuzz(start, end)
