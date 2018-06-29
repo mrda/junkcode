8f73084933041fcaa83ffcb34fc472e346bad4c9
Michael Davies <michael@the-davies.net>
2018-06-29 15:46:33 +0930

    Example code with tests

--- roman.py
+++ roman.py
@@ -21,8 +21,13 @@
 # Or try here: http://www.fsf.org/copyleft/gpl.html
 #
 from __future__ import print_function
+import getopt
 import os
 import sys
+import unittest
+
+
+valid_opts = ['help', 'self-test', 'verbose']
 
 
 roman_numbers = {
@@ -67,6 +72,7 @@ def follow_on_digit(number, idx):
 
 
 def convert_roman(number):
+    # TODO(mrda): Add invalid number check
     result = 0
     skip = False
     for idx, roman_digit in enumerate(number):
@@ -89,6 +95,7 @@ def convert_roman(number):
 
 
 def is_roman(number):
+    # TODO(mrda): Add invalid number check
     for digit in number:
         if digit not in roman_numbers:
             return False
@@ -108,12 +115,90 @@ def convert_roman_arabic(number):
         return "Unknown number format"
 
 
-def print_usage():
-    print("{}: <roman or arabic number>".format(os.path.basename(__file__)))
+def exit_with_usage(code=1):
+    sys.stderr.write("Usage: {0} [{1}] <roman or arabic number>\n"
+                     .format(os.path.basename(sys.argv[0]),
+                             '|'.join('--'+opt for opt in valid_opts)))
+    sys.exit(code)
+
+
+#
+# Tests
+#
+
+class TestRoman(unittest.TestCase):
+
+    def test_roman_simple(self):
+        self.assertEqual(1, convert_roman('I'))
+        self.assertEqual(4, convert_roman('IV'))
+        self.assertEqual(5, convert_roman('V'))
+        self.assertEqual(9, convert_roman('IX'))
+        self.assertEqual(10, convert_roman('X'))
+        self.assertEqual(40, convert_roman('XL'))
+        self.assertEqual(50, convert_roman('L'))
+        self.assertEqual(90, convert_roman('XC'))
+        self.assertEqual(100, convert_roman('C'))
+        self.assertEqual(400, convert_roman('CD'))
+        self.assertEqual(500, convert_roman('D'))
+        self.assertEqual(900, convert_roman('CM'))
+        self.assertEqual(1000, convert_roman('M'))
+
+    def test_roman_tricky(self):
+        self.assertEqual(99, convert_roman('XCIX'))
+        self.assertEqual(999, convert_roman('CMXCIX'))
+
+    def test_symmetric(self):
+        for i in range(1, 5000):
+            first = convert_arabic(i)
+            second = convert_roman(first)
+            self.assertEqual(i, second)
+
+    def test_roman_years(self):
+        self.assertEqual(1982, convert_roman('MCMLXXXII'))
+        self.assertEqual(2000, convert_roman('MM'))
+
+    def test_arabic_years(self):
+        self.assertEqual('LVI', convert_arabic(56))
+        self.assertEqual('MMXVIII', convert_arabic(2018))
+
+    def test_identity(self):
+        self.assertFalse(is_arabic('XXX'))
+        self.assertFalse(is_roman('1979'))
+        self.assertTrue(is_roman('XXX'))
+        self.assertTrue(is_arabic('1979'))
+
+    def test_incorrect_roman(self):
+        self.assertFalse(is_roman('IC'))
+        self.assertEqual("Unknown number format", convert_roman('IC'))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print_usage()
+
+    runtests = False
+
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], '', valid_opts)
+    except getopt.error:
+        exit_with_usage()
+
+    opt_flags = [flag for (flag, val) in opts]
+    for opt in opt_flags:
+        if opt == '--verbose':
+            print("verbsoing")
+            debug = True
+        elif opt == '--help':
+            exit_with_usage(code=0)
+        elif opt == '--self-test':
+            runtests = True
+
+    if runtests:
+        del sys.argv[1:]
+        if args:
+            print("Ignoring arguments, and just running tests")
+        unittest.main()
+        sys.exit(0)
+
+    if len(args) != 1:
+        exit_with_usage()
     else:
-        print(convert_roman_arabic(sys.argv[1]))
+        print(convert_roman_arabic(args[0]))
