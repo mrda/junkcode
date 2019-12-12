8433fc79eff1b1e4c772e0f4f89e2ee114d27cb9
Michael Davies <michael@the-davies.net>
2019-12-13 10:23:10 +1030

    Bug fix & refactor

--- poll-internet.sh
+++ poll-internet.sh
@@ -20,69 +20,108 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 #
-HOSTS_TO_CHECK=( google.com facebook.com twitter.com )
+HOSTS_TO_CHECK=( google-public-dns-a.google.com
+                 google-public-dns-b.google.com
+                 one.one.one.one
+                 ns1.telstra.net
+                 facebook.com
+                 twitter.com
+                 akamai.com )
+
 POLL_NORMAL=300 # seconds
 POLL_OUTAGE=60  # seconds
 
+OUTAGE_FILE=${HOME}/.$(basename $0).log
+
+# Use DEBUG from the environment, but ensure that we don't error if it's not
+[[ -z "$DEBUG" ]] && DEBUG=0
+
+# USB notification dongle https://blink1.thingm.com/blink1-tool/
 BLINK=blink1-tool
 HAVEBLINK=0
 if hash $BLINK &> /dev/null; then
     HAVEBLINK=1
 fi
 
-declare -a IP_ADDR_LIST
-for NAME in ${HOSTS_TO_CHECK[*]}; do
-    IP_ADDR_LIST+=( $(dig +short $NAME | head -n 1) )
-done
-
-
-show_time ()
-{
-    # Display the number of seconds supplied in a user friendly
-    # format of "d h m s"
-    SECS=$1
-    printf "%0dd %0dh %0dm %0ds\n" $(($SECS/86400)) $(($SECS%86400/3600)) $(($SECS%3600/60)) $(($SECS%60))
-}
-
 blink ()
 {
-    if [[ $HAVEBLINK -eq 1 ]]; then
-        $BLINK --blue >& /dev/null
-    fi
+    [[ $HAVEBLINK -eq 1 ]] && $BLINK --blue >& /dev/null
 }
 
 clear_blink ()
 {
-    if [[ $HAVEBLINK -eq 1 ]]; then
-        $BLINK --off >& /dev/null
-    fi
+    [[ $HAVEBLINK -eq 1 ]] && $BLINK --off >& /dev/null
 }
 
+# Display the number of seconds supplied in a user friendly format of "d h m s"
+show_time ()
+{
+    SECS=$1
+    printf "%0dd %0dh %0dm %0ds\n" $(($SECS/86400)) $(($SECS%86400/3600)) $(($SECS%3600/60)) $(($SECS%60))
+}
+
+
+# Check a list of HA internet sites via a single ICMP packet, seeing if
+# _any_ of them are reachable. If at least one is, our internet must be ok.
 is_internet_up ()
 {
     ANY_OK=0
     for IP_ADDR in ${IP_ADDR_LIST[*]}; do
+        [[ $DEBUG -eq 1 ]] && echo $(date +"%Y%m%d %H:%M:%S ") Checking $IP_ADDR
         ping -c1 $IP_ADDR >& /dev/null
         if [ $? -eq 0 ]; then
             ANY_OK=1
+            break
         fi
     done
 
-    if [ $ANY_OK -eq 1 ]; then
-        return 0
-    fi
-    return 1
+    return $ANY_OK
 }
 
+# Shuffle an array, call like this:
+# array_shuffle ARRAY_TO_SHUFFLE ${ARRAY_TO_SHUFFLE[@]}
+array_shuffle ()
+{
+    local OUTPUT=$1
+    local INPUT=("${@:2}")
+
+    local NUM=$(( ${#INPUT[@]} -1 ))
+    local NEWSEQ=$( shuf -i 0-$NUM )
+    declare -a NEWARR
+    for i in $NEWSEQ; do
+        NEWARR+=( ${INPUT[$i]} )
+    done
+
+    eval unset $OUTPUT
+    eval $OUTPUT="'${NEWARR[*]}'"
+}
+
+#
+# Main
+#
+printf "<<< Restarting %s at %s >>>\n" $(basename $0) "$(date +'%Y%m%d %H:%M:%S')" >> $OUTAGE_FILE
+
+# Translate hostnames into IP addresses, and perform IP address lookup once
+declare -a IP_ADDR_LIST
+for NAME in ${HOSTS_TO_CHECK[*]}; do
+    IP_ADDR=( $(dig +short $NAME | head -n 1) )
+    if [[ -z ${IP_ADDR} ]]; then
+        printf "%s: Unable to resolve hostname. Exiting...\n" $(basename $0)
+        exit 1
+    fi
+    IP_ADDR_LIST+=( $IP_ADDR )
+
+    [[ $DEBUG -eq 1 ]] && printf "%s has IP Address %s\n" $NAME $IP_ADDR
+done
 
 while true; do
 
+    # Shuffle IP_ADDR_LIST so we don't hit the same host all the time
+    array_shuffle IP_ADDR_LIST ${IP_ADDR_LIST[@]}
+
     OUTAGE=0
     while true; do
-        is_internet_up
-        if [ $? -eq 0 ]; then
-            break
-        fi
+        is_internet_up || break
         if [ $OUTAGE -eq 0 ]; then
             OUTAGE=1
             START="$(date +%s)"
@@ -100,6 +139,7 @@ while true; do
         if [ $DIFF -gt 0 ]; then
             echo -n " Down for "
             show_time $DIFF
+            printf "%s,%s,%s\n" $START $END $DIFF >> $OUTAGE_FILE
         fi
     fi
 
