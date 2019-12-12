4d25bbf579c636e3eb7d99d55d7d1f0807df67f8
Michael Davies <michael@the-davies.net>
2019-12-13 09:50:48 +1030

    Bash array shuffle

---
+++ bashshuf.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+#
+# bashshuf - Example script to show how to shuffle an array
+#            Invoke via `array_shuffle ARRAYNAME`.
+#
+#            i.e. you just provide the NAME of the array,
+#            and the functions grabs the contents of the
+#            array and overwrites it with the shuffled
+#            contents.
+#
+# Copyright (C) 2019 Michael Davies <michael@the-davies.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+array_shuffle ()
+{
+    local OUTPUT=$1
+
+    local ARRAYNAME=$1[@]
+    local INPUTARRAY=( ${!ARRAYNAME} )
+
+    local NUM=$(( ${#INPUTARRAY[@]} -1 ))
+    local NEWSEQ=$( shuf -i 0-$NUM )
+    declare -a NEWARR
+    for i in $NEWSEQ; do
+        NEWARR+=( ${INPUTARRAY[$i]} )
+    done
+
+    eval unset $OUTPUT
+    eval $OUTPUT="'${NEWARR[*]}'"
+}
+
+if [[ $1 == "-h" ]]; then
+    printf "Usage: %s -h | [<list of items to shuffle>]\n" $(basename $0)
+    exit 1
+fi
+
+if [[ $# -eq 0 ]]; then
+    printf "%s: No list provided, supplying an example\n" $(basename $0)
+    MYARR=( john paul george ringo )
+else
+    MYARR=( "$@" )
+fi
+
+array_shuffle MYARR
+printf "${MYARR[*]}\n"
