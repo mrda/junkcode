0f403164c1d5485e279e14df6aece96e16f2ddb6
Michael Davies <michael@the-davies.net>
2022-01-19 15:53:05 +1030

    Just a little more cleanup

--- wordle.py
+++ wordle.py
@@ -24,9 +24,31 @@
 
 import argparse
 import re
+import string
 import sys
 
 thedict = []
+
+def only_az(word):
+    """Does word only contain characters in [a..z]?"""
+    for char in word:
+        if char not in string.ascii_lowercase:
+            return False
+    return True
+
+def contains_all(strng, setofchars):
+    """Does strng contain all of the chars in setofchars?"""
+    for char in setofchars:
+        if char not in strng:
+            return False
+    return True
+
+def contains_any(strng, setofchars):
+    """Does strng contain any of the chars in setofchars?"""
+    for char in setofchars:
+        if char in strng:
+            return True
+    return False
 
 def load_dict():
     """Load up the system dictionary"""
@@ -34,92 +56,86 @@ def load_dict():
     with open('/usr/share/dict/words', 'r', encoding="utf-8") as dictfile:
         for line in dictfile:
             word = line.strip().lower()
-            if len(word) == 5:
+            if len(word) == 5 and only_az(word):
                 thedict.append(word)
                 num += 1
     if args.verbose:
         print(f"Initial dictionary has a total of {num} five letter words")
 
-def contains_all(strng, setofchars):
-    """Does strng contain all of the chars in setofchars?"""
-    for char in setofchars:
-        if char not in strng:
-            return 0
-    return 1
-
-def contains_any(strng, setofchars):
-    """Does strng contain any of the chars in setofchars?"""
-    for char in setofchars:
-        if char in strng:
-            return 1
-    return 0
-
 parser = argparse.ArgumentParser(
     description='Find possible matches for wordle.',
     epilog='You can find Wordle here: https://www.powerlanguage.co.uk/wordle/')
-parser.add_argument('--excluded', type=str, dest='excluded',
-                    help="Letters to be excluded")
-parser.add_argument('--letters', type=str, dest='letters',
-                    help="Known letters")
-parser.add_argument('--matches', type=str, dest='matches',
-                    help="Known letter positions, e.g. ....t is a 5 letter"
+parser.add_argument('--exclude', type=str, dest='exclude',
+                    help="letters to be excluded")
+parser.add_argument('--include', type=str, dest='include',
+                    help="letters to be included")
+parser.add_argument('--match', type=str, dest='match',
+                    help="known letter positions, e.g. ....t is a 5 letter"
                          " word ending in 't'")
 parser.add_argument('-v', '--verbose',
                     action='store_true',
                     dest='verbose',
-                    help='Display extra information')
+                    help='display extra information')
 args = parser.parse_args()
 
 load_dict()
 thedict = list(set(thedict))  # Remove dups because there's a bug in my code
 
-if (args.letters is None and
-    args.matches is None and
-    args.excluded is None):
+if (args.include is None and
+    args.match is None and
+    args.exclude is None):
+
+    thedict.sort()
     for entry in thedict:
         print(entry)
 else:
 
-    # Any or all of the --letters, --matches, and --excludes might be present
+    # Any or all of the --include, --match, and --exclude might be present
 
-    # Let's remove words with --excluded letters
-    if args.excluded:
+    # Let's remove words with --exclude letters
+    if args.exclude:
         excluded_matches = []
         COUNT = 0
         for entry in thedict:
-            if not contains_any(entry, args.excluded):
+            if not contains_any(entry, args.exclude):
                 excluded_matches.append(entry)
                 COUNT += 1
         thedict = excluded_matches
         if args.verbose:
             print(f"Removing words that contain letters "
-                  f"'{args.excluded}' reduces possible matches down to "
+                  f"'{args.exclude}' reduces possible matches down to "
                   f"{COUNT} words.")
 
-    # Remove any entries that don't have all of the --letters
-    if args.letters:
+    # Remove any entries that don't have all of the --include letters
+    if args.include:
+
+        # Sanity check
+        if len(args.include) > 5:
+            print("Error: You can't have more than 5 included letters. Exiting...")
+            sys.exit(1)
+
         restricted_matches = []
         COUNT = 0
         for entry in thedict:
-            if contains_all(entry, args.letters):
+            if contains_all(entry, args.include):
                 restricted_matches.append(entry)
                 COUNT += 1
         thedict = restricted_matches
         if args.verbose:
-            print(f"Requiring this set of letters '{args.letters}' reduces "
+            print(f"Requiring this set of letters '{args.include}' reduces "
                   f"possible matches down to {COUNT} words.")
 
     # Try and find a regex match for correctly positioned letters
-    if args.matches:
+    if args.match:
 
-        # Sanity
-        if len(args.matches) != 5:
+        # Sanity check
+        if len(args.match) != 5:
             print("Error: Match string must be 5 chars long. Exiting...")
             sys.exit(1)
 
         # Build up the match string
         MATCH_RE = "^"
-        for ch in args.matches:
+        for ch in args.match:
             if ch == '.':
                 MATCH_RE += "[a-z]"
             else:
@@ -140,5 +156,6 @@ else:
     # Print out the candidates
     if args.verbose:
         print("Candidate words:")
+    thedict.sort()
     for possibility in thedict:
         print(possibility)
