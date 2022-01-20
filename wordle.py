fb996e7eb80289c4bac23863fb6c1b65e13ad63b
Michael Davies <michael@the-davies.net>
2022-01-20 12:10:17 +1030

    Refactored into functions

--- wordle.py
+++ wordle.py
@@ -20,7 +20,7 @@
 # Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
 # 02111-1307, USA.
 #
-""" Display Wordle matches """
+""" Provide potential solutions for Wordle """
 
 import argparse
 import re
@@ -28,7 +28,6 @@ import string
 import sys
 
 DEFAULT_WORD_LEN = 5
-thedict = []
 
 def only_az(word):
     """Does word only contain characters in [a..z]?"""
@@ -51,7 +50,7 @@ def contains_any(strng, setofchars):
             return True
     return False
 
-def regex_reduce(matchstr, antimatch=False):
+def regex_reduce(mydict, matchstr, antimatch=False):
     """ Return the dictionary based upon the supplied matchstr.
         matchstr is a pseudo-regex, i.e. ..o.. is a 5 letter word with
         an o in the 3rd spot.  antimatch determines whether this is a
@@ -78,7 +77,7 @@ def regex_reduce(matchstr, antimatch=False):
 
     reduced_words = []
     matches = 0
-    for possible_match in thedict:
+    for possible_match in mydict:
         if re.match(match_re, possible_match):
             reduced_words.append(possible_match)
             matches += 1
@@ -90,6 +89,7 @@ def regex_reduce(matchstr, antimatch=False):
 def load_dict():
     """Load up the system dictionary"""
     num = 0
+    thedict = []
     with open('/usr/share/dict/words', 'r', encoding="utf-8") as dictfile:
         for line in dictfile:
             word = line.strip().lower()
@@ -98,91 +98,99 @@ def load_dict():
                 num += 1
     if args.verbose:
         print(f"Initial dictionary has a total of {num} five letter words")
+    return thedict
 
-parser = argparse.ArgumentParser(
-    description='Find possible matches for wordle.',
-    epilog='You can find Wordle here: https://www.powerlanguage.co.uk/wordle/')
-parser.add_argument('-e', '--exclude', type=str, dest='exclude',
-                    help="letters to be excluded")
-parser.add_argument('-i', '--include', type=str, dest='include',
-                    help="letters to be included")
-parser.add_argument('-m', '--match', type=str, dest='match',
-                    help="known letter positions, e.g. ....t is a 5 letter"
-                         " word ending in 't'")
-parser.add_argument('-a', '--antimatch', type=str, dest='antimatch',
-                    help="where certain letters aren't, e.g. ...o. is a 5 letter"
-                         " word, and the 4th letter isn't a 'o'")
-parser.add_argument('-n', '--numchars', type=int, dest='word_len',
-                    default=DEFAULT_WORD_LEN,
-                    help=f"word length to consider, "
-                         f"default to {DEFAULT_WORD_LEN}")
-parser.add_argument('-v', '--verbose',
-                    action='store_true',
-                    dest='verbose',
-                    help='display extra information')
-args = parser.parse_args()
-
-load_dict()
-thedict = list(set(thedict))  # Remove dups because there's a bug in my code
-
-if (args.include is None and
-    args.match is None and
-    args.antimatch is None and
-    args.exclude is None):
-
-    thedict.sort()
-    for entry in thedict:
-        print(entry)
-else:
-
-    # Any or all of the --include, --exclude, --match, and --antimatch
-    # might be present
+def wordle(mydict, include=None, exclude=None, match=None, antimatch=None):
+    """ Find wordle candidate words, using the provided dictionary 'mydict'.
+    include is a string of letters that must be in the word
+    exclude is a string of letters that must NOT be in the word
+    match is a pseudo-regex on letters in the correct places,
+      i.e. ..o.. is a 5 letter word with an o in the 3rd spot.
+    antimatch is a pseudo-regex on letters that must NOT be in a certain slot.
+      i.e. .a... is a 5 letter word that mustn't have an 'a' in the 2nd slot."""
 
     # Let's remove words with --exclude letters
-    if args.exclude:
+    if exclude:
         excluded_matches = []
-        COUNT = 0
-        for entry in thedict:
-            if not contains_any(entry, args.exclude):
+        count = 0
+        for entry in mydict:
+            if not contains_any(entry, exclude):
                 excluded_matches.append(entry)
-                COUNT += 1
-        thedict = excluded_matches
+                count += 1
+        mydict = excluded_matches
         if args.verbose:
             print(f"Removing words that contain letters "
-                  f"'{args.exclude}' reduces possible matches down to "
-                  f"{COUNT} words.")
+                  f"'{exclude}' reduces possible matches down to "
+                  f"{count} words.")
 
     # Remove any entries that don't have all of the --include letters
-    if args.include:
-
+    if include:
         # Sanity check
-        if len(args.include) > args.word_len:
+        if len(include) > args.word_len:
             print(f"Error: You can't have more than {args.word_len} included "
                   f"letters. Exiting...")
             sys.exit(1)
 
         restricted_matches = []
-        COUNT = 0
-        for entry in thedict:
-            if contains_all(entry, args.include):
+        count = 0
+        for entry in mydict:
+            if contains_all(entry, include):
                 restricted_matches.append(entry)
-                COUNT += 1
-        thedict = restricted_matches
+                count += 1
+        mydict = restricted_matches
         if args.verbose:
-            print(f"Requiring this set of letters '{args.include}' reduces "
-                  f"possible matches down to {COUNT} words.")
+            print(f"Requiring this set of letters '{include}' reduces "
+                  f"possible matches down to {count} words.")
 
     # Try and find a regex --match for correctly positioned letters
-    if args.match:
-        thedict = regex_reduce(args.match, antimatch=False)
+    if match:
+        mydict = regex_reduce(mydict, match, antimatch=False)
 
     # Let's remove candidates according to the --antimatch regular expression
-    if args.antimatch:
-        thedict = regex_reduce(args.antimatch, antimatch=True)
+    if antimatch:
+        mydict = regex_reduce(mydict, antimatch, antimatch=True)
 
     # Print out the candidates
     if args.verbose:
         print("Candidate words:")
-    thedict.sort()
-    for possibility in thedict:
-        print(possibility)
+    mydict.sort()
+    return mydict
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+                description='Find possible matches for wordle.',
+                epilog='You can find Wordle here: https://www.powerlanguage.co.uk/wordle/')
+    parser.add_argument('-e', '--exclude', type=str, dest='exclude',
+                        help="letters to be excluded")
+    parser.add_argument('-i', '--include', type=str, dest='include',
+                        help="letters to be included")
+    parser.add_argument('-m', '--match', type=str, dest='match',
+                        help="known letter positions, e.g. ....t is a 5 letter"
+                        " word ending in 't'")
+    parser.add_argument('-a', '--antimatch', type=str, dest='antimatch',
+                        help="where certain letters aren't, e.g. ...o. is a 5 letter"
+                             " word, and the 4th letter isn't a 'o'")
+    parser.add_argument('-n', '--numchars', type=int, dest='word_len',
+                        default=DEFAULT_WORD_LEN,
+                        help=f"word length to consider, "
+                        f"default to {DEFAULT_WORD_LEN}")
+    parser.add_argument('-v', '--verbose',
+                        action='store_true',
+                        dest='verbose',
+                        help='display extra information')
+    args = parser.parse_args()
+
+    if (args.include is None and
+        args.match is None and
+        args.antimatch is None and
+        args.exclude is None):
+
+        parser.print_help()
+        sys.exit(0)
+
+    # TODO(mrda): Fix duplicate entry bug in load_dict
+    dictionary = list(set(load_dict()))
+
+    for candidate in wordle(dictionary, args.include, args.exclude,
+                            args.match, args.antimatch):
+        print(candidate)
