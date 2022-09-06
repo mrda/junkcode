86c673a1191a948c5ed7bb0604ef06a3a2bdd893
Michael Davies <michael@the-davies.net>
2022-09-06 17:26:55 +0930

    Shortcut to find IPs

---
+++ virships.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# virships.sh - perint ip addresses associated with virsh vms
+#
+# Copyright (C) 2022 Michael Davies <michael@the-davies.net>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+# 02111-1307, USA.
+#
+
+#CONNECT="-c 'qemu:///system'"
+CONNECT=
+
+VM_NAMES=( $(virsh ${CONNECT} list | awk 'NR>2 { print $2 }') )
+for VM in ${VM_NAMES[*]}; do
+    IPADDRS=( $(virsh ${CONNECT} domifaddr ${VM} | awk 'NR>2 { print $4 }' | cut -f1 -d'/') )
+    printf "${VM} ${IPADDRS[*]}\n"
+done
